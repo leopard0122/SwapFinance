ca9d3d73a67d72094b49631599917e2c7dc9c5b4
leopard0122 <leopard.live0122@gmail.com>
2023-03-04 11:11:52 -0500

    'movedToPub'

---
+++ src/components/Menu/config.ts
@@ -0,0 +1,124 @@
+import { MenuEntry } from '@apeswapfinance/uikit'
+
+export const BASE_APP_URL = process.env.REACT_APP_BASE_APP_URL || 'https://apeswap-frontend.vercel.app';
+
+const config: MenuEntry[] = [
+  {
+    label: 'Home',
+    icon: 'HomeIcon',
+    href: BASE_APP_URL,
+  },
+  {
+    label: 'Ape Stats',
+    icon: 'StatsIcon',
+    href: `${BASE_APP_URL}/stats`,
+  },
+  {
+    label: 'Trade',
+    icon: 'TradeIcon',
+    initialOpenState: true,
+    items: [
+      {
+        label: 'Exchange',
+        href: '/swap',
+      },
+      {
+        label: 'Liquidity',
+        href: '/pool',
+      },
+    ],
+  },
+  {
+    label: 'Farms',
+    icon: 'FarmIcon',
+    href: `${BASE_APP_URL}/farms`,
+  },
+  {
+    label: 'Pools',
+    icon: 'PoolIcon',
+    href: `${BASE_APP_URL}/pools`,
+  },
+  {
+    label: 'IAO',
+    icon: 'IfoIcon',
+    href: `${BASE_APP_URL}/iao`,
+  },
+  {
+    label: 'NFA',
+    icon: 'apeNFTIcon',
+    href: `${BASE_APP_URL}/nft`,
+  },
+  {
+    label: 'ApeZone',
+    icon: 'ApeZone',
+    href: `${BASE_APP_URL}/apezone`,
+  },
+  // {
+  //   label: 'Lottery',
+  //   icon: 'TicketIcon',
+  //   href: `${BASE_APP_URL}/lottery`,
+  // },
+  {
+    label: 'Info',
+    icon: 'InfoIcon',
+    items: [
+      {
+        label: 'Overview',
+        href: 'https://info.apeswap.finance',
+      },
+      {
+        label: 'Tokens',
+        href: 'https://info.apeswap.finance/tokens',
+      },
+      {
+        label: 'Pairs',
+        href: 'https://info.apeswap.finance/pairs',
+      },
+      {
+        label: 'Accounts',
+        href: 'https://info.apeswap.finance/accounts',
+      },
+    ],
+  },
+  {
+    label: 'More',
+    icon: 'MoreIcon',
+    items: [
+      {
+        label: 'Github',
+        href: 'https://github.com/apeswapfinance',
+      },
+      {
+        label: 'Docs',
+        href: 'https://apeswap.gitbook.io/apeswap-finance',
+      },
+      {
+        label: 'Blog',
+        href: 'https://ape-swap.medium.com',
+      },
+      {
+        label: 'Team',
+        href: 'https://ape-swap.medium.com/meet-the-founding-monkeys-behind-apeswap-6f837113db00',
+      },
+      {
+        label: 'Community Feedback',
+        href: 'https://apeswap.nolt.io/',
+      },
+      {
+        label: 'ApeTV',
+        href: 'https://anchor.fm/apetv',
+      },
+      {
+        label: 'Bug Bounty Program',
+        href: 'https://apeswap.gitbook.io/apeswap-finance/security/bug-bounty-program',
+      },
+      {
+        label: 'GEMZ Audit Report',
+        href:
+          'https://github.com/ApeSwapFinance/apeswap-banana-farm/blob/master/audits/ApeSwap_GEMZ_Audit_Report_21.03.05.pdf',
+      },
+    ],
+  },
+]
+
+export default config
---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,47 @@
+import React, { useContext } from 'react'
+import { Menu as UikitMenu, ConnectorNames } from '@apeswapfinance/uikit'
+import { useWeb3React } from '@web3-react/core'
+import { LanguageContext } from 'hooks/LanguageContext'
+import useTheme from 'hooks/useTheme'
+import { useGetPriceDataFromLP } from 'hooks/useGetPriceData'
+import useGetLocalProfile from 'hooks/useGetLocalProfile'
+import { injected, bsc, walletconnect } from 'connectors'
+import links from './config'
+
+const Menu: React.FC = (props) => {
+  const { account, activate, deactivate } = useWeb3React()
+  const { selectedLanguage, setSelectedLanguage } = useContext(LanguageContext)
+  const { isDark, toggleTheme } = useTheme()
+  // const priceData = useGetPriceData()
+  // const cakePriceUsd = priceData ? Number(priceData.prices.Cake) : undefined
+  const lpPrice = useGetPriceDataFromLP()
+  const profile = useGetLocalProfile()
+
+  return (
+    <UikitMenu
+      links={links}
+      account={account as string}
+      login={(connectorId: ConnectorNames) => {
+        if (connectorId === 'walletconnect') {
+          return activate(walletconnect)
+        }
+
+        if (connectorId === 'bsc') {
+          return activate(bsc)
+        }
+
+        return activate(injected)
+      }}
+      logout={deactivate}
+      isDark={isDark}
+      toggleTheme={toggleTheme}
+      currentLang={selectedLanguage?.code || ''}
+      setLang={setSelectedLanguage}
+      cakePriceUsd={lpPrice}
+      profile={profile}
+      {...props}
+    />
+  )
+}
+
+export default Menu
